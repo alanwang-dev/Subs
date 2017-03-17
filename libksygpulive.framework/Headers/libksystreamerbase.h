01abed4f0cd4a2173e7da351655ef1574ebd85ca
wangkecheng <wangkecheng@kingsoft.com>
2017-03-17 11:54:10 +0800

    cg oppu of AUnodeInfo initial

---
+++ libksygpulive.framework/Headers/libksystreamerbase.h
@@ -0,0 +1,16 @@
+//
+//  libksystreamerbase.h
+//  libksystreamerbase
+//
+//  Copyright (c) 2016 ksyun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "KSYTypeDef.h"
+#import "KSYStreamerBase.h"
+#import "KSYMovieWriter.h"
+#import "KSYAVMuxer.h"
+#import "KSYMessage.h"
+
+//#define KSYSTREAMERBASE_VER 2.0.2
+//#define KSYSTREAMERBASE_ID 715f3368cc54848296c0820ebb5845cb2124df17
---
+++ libksygpulive.framework/Headers/libksystreamerengine.h
@@ -0,0 +1,31 @@
+//
+//  libksystreamerengine.h
+//  libksystreamerengine.h
+//
+//  Copyright (c) 2016 ksyun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import <GPUImage/GPUImage.h>
+
+// sources (player & capture)
+#import "KSYGPUPicInput.h"
+#import "KSYGPUCamera.h"
+#import "AVAudioSession+KSY.h"
+#import "KSYAVAudioSession.h"
+#import "KSYBgmPlayer.h"
+#import "KSYBgmReader.h"
+#import "KSYAQPlayer.h"
+#import "KSYAUAudioCapture.h"
+#import "KSYDummyAudioSource.h"
+#import "KSYAVFCapture.h"
+// mixer
+#import "KSYGPUPicMixer.h"
+#import "KSYAudioMixer.h"
+// streamer
+#import "KSYGPUPicOutput.h"
+
+
+#define KSYSTREAMERENGINE_VER 2.0.2
+#define KSYSTREAMERENGINE_ID 8ef163210af8beb674e1a0dc581cb57f585fa72c
